9f2906cd66f7138e6f9902d7cf7535ed700bba5b
shamila p <lulushamila@gmail.com>
2023-04-13 19:41:52 +0530

    folder structure changed

--- wizardapi/urls.py
+++ wizardapi/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('wizardapi/v1/', include('api.urls')),
+    path('wizardapi/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
